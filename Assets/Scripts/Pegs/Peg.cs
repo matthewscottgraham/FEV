578ef2ca9ae00c8f32da581340fc2e8a9db0bd76
matthewscottgraham <mggamedevs@gmail.com>
2025-09-10 14:56:27 +1000

    Added an outline effect to higher scoring pegs

--- Assets/Scripts/Pegs/Peg.cs
+++ Assets/Scripts/Pegs/Peg.cs
@@ -12,10 +12,15 @@ namespace Pegs
     {
         private SpriteRenderer _spriteRenderer;
         private Tween _tween;
+        private int _score = 1;
         private float _randomDelay = 0.1f;
         private bool _isDeactivated = false;
 
-        public int Score { get; set; } = 1;
+        public int Score
+        {
+            get => _score;
+            set => SetScore(value);
+        }
         public PegState PegState { get; private set; }
         public Player Owner { get; private set; }
         public IEffect Effect { get; private set; }
@@ -73,6 +78,16 @@ namespace Pegs
             _isDeactivated = true;
             SetState();
             SetMaterial();
+        }
+
+        private void SetScore(int score)
+        {
+            _score = score;
+            if (_score > 1)
+            {
+                var pegShadow = gameObject.AddComponent<PegShadow>();
+                pegShadow.Initialize(_spriteRenderer);
+            }
         }
 
         private void SetState(bool isHighlighted = false, bool isValidHighlight = false, bool canBePlacedOnObstructions = false)
